178b72b078b73cb47a947870777c8750aeb651e5
edevars <e.devars.dev@gmail.com>
2021-05-13 14:55:27 -0500

    :sparkles: Disconnecting frontend

--- routes/render/games.js
+++ routes/render/games.js
@@ -1,11 +1,10 @@
-const GameService = require('../../services/game')
+const games = require('../../utils/data/games')
 
 function GamesRender(app) {
-    const gameService = new GameService()
 
-    app.get('/', async function (req, res, next) {
+    app.get('/', function (req, res, next) {
         try {
-            const games = await gameService.getGames()
+            
             res.render("games", { games })
         } catch (error) {
             next(error)
